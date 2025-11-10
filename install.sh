cb83f3628a6c80d2cde3ec04a4ef073ab3905421
neonlangoff-ops <neonlangoff@gmail.com>
2025-11-10 14:28:26 -0500

    New aspect

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# 🌌 Termux Setup Script by Shafow-TermDev
+# 🌌 Termux Setup Script by Shadow-TermDev
 # Instalador automatizado para entornos de desarrollo en Termux
 
 set -e
